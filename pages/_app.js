870ac6a5e7f0f4de1e2bb19da73a11620026ba42
Abbosjon7788 <nosirovabosjon3429@gmail.com>
2022-04-27 13:15:21 +0500

    movie info in process

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,9 @@
-import { useEffect, useRef } from 'react'
-import { useRouter } from 'next/router'
 import Layout from 'components/layout'
 import 'styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const pageRef = useRef()
-
-  // useEffect(() => {
-  //   console.log('page-ref', pageRef.current.offsetTop)
-  //   const handleRouteChange = (url, { shallow }) => {
-  //     pageRef.current.scrollIntoView()
-  //   }
-  //   window.scrollTo(0, pageRef.current.offsetTop)
-  //   // pageRef.current.scrollIntoView()
-  //   // router.events.on('routeChangeStart', () => pageRef.current.scrollIntoView())
-  //   return () => {
-  //     // router.events.off('routeChangeStart', () => pageRef.current.scrollIntoView())
-  //     // pageRef.current.scrollIntoView()
-  //     window.scrollTo(0, pageRef.current.offsetTop)
-  //   }
-  // }, [router.asPath])
-
   return (
-    <Layout pageRef={pageRef}>
+    <Layout>
       <Component {...pageProps} />
     </Layout>
   )
--- pages/movie/[movid].js
+++ pages/movie/[movid].js
@@ -1,4 +1,5 @@
-const MovieInfo = () => {
+const MovieInfo = ({ data }) => {
+     console.log('data', data?.movie)
      return (
           <div className="movie-info">
                hello
@@ -7,3 +8,27 @@ const MovieInfo = () => {
 }
 
 export default MovieInfo;
+
+export const getStaticProps = async ({ params }) => {
+     const { API_URL, TOKEN } = process.env
+     const res = await fetch(`${API_URL}/content/main/2/show/${params.movid}?user=${TOKEN}`)
+     const { data } = await res.json()
+     return {
+          props: { data }
+     }
+}
+
+export const getStaticPaths = async () => {
+     const { API_URL, TOKEN } = process.env
+     const res = await fetch(`${API_URL}/content/main/2/list?user=${TOKEN}`)
+     const { data } = await res.json()
+     const paths = data?.movies.map(item => {
+          return {
+               params: { movid: `${item.id}` }
+          }
+     })
+     return {
+          paths,
+          fallback: false
+     }
+}
